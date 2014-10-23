ce84960d18befb9e14cc50c37517752eb9c3e6dc
Lawrence L <none@example.com>
2014-10-22 18:12:19 -0700

    Forgot to add other files besdies readme.

---
+++ src/main/java/com/euler/Problem1.java
@@ -0,0 +1,32 @@
+/** *************************************************
+ * Project Euler problem 1: multiples of 3 and 5.
+ *
+ * Description:
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+ * we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ * ************************************************** */
+package com.euler;
+
+import java.util.ArrayList;
+
+public class Problem1{
+
+    /** *************************************************
+     * ************************************************** */
+    public static ArrayList<Integer> findMultiples(int max){
+        ArrayList<Integer> results = new ArrayList<>();
+
+        int a = 3, b = 5;
+
+        return results;
+    }
+
+    public static void main(String args[]){
+        findMultiples(100);
+        findMultiples(1000);
+
+        System.out.println("Maven is setup to build Project Euler programs.");
+    }
+}
